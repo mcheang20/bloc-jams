31edeb4730b501446cf0318176eb029e47317bc3
mattcheang <cheangmatt@gmail.com>
2016-08-01 16:37:13 -0700

    checkpoint 26 complete

--- scripts/album.js
+++ scripts/album.js
@@ -63,24 +63,38 @@ var setCurrentAlbum = function(album) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  
- var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
- var songRows = document.getElementsByClassName('album-view-song-item');
-
- var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
      
-        songListContainer.addEventListener('mouseover', function(event) {
+      songListContainer.addEventListener('mouseover', function(event) {
             if (event.target.parentElement.className === 'album-view-song-item') {
-              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-            }
+                event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
      });
-             for (var i = 0; i < songRows.length; i++) {
+     
+      for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
-             // Selects first child element, which is the song-item-number element
-              this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
          });
      }
  }
+
+
+var findParentByCLassName = function(element, parentClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+         while (currentParent.className != parentClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
+        return currentParent;
+    }
+};
+
+var getSongItem = function
+    
